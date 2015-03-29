919710b37e801ff1020def54ac6f241e57e66a17
Rick van Hattem <Wolph@wol.ph>
2015-03-29 18:57:45 +0200

    enabied python 3.4 support

--- mt940/__init__.py
+++ mt940/__init__.py
@@ -1,3 +1,5 @@
-from parser import parse
+from __future__ import absolute_import
+
+from .parser import parse
 
 __all__ = ['parse']
--- mt940/metadata.py
+++ mt940/metadata.py
@@ -1,5 +1,5 @@
 __package_name__ = 'mt-940'
-__version__ = '1.0'
+__version__ = '1.0.1'
 __author__ = 'Rick van Hattem'
 __author_email__ = 'Wolph@Wol.ph'
 __description__ = '''
