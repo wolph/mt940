65d106895f01d8b11ab1436bd79d6ada49437636
Rick van Hattem <Wolph@wol.ph>
2019-01-10 03:26:17 +0100

    fixed tests for the entry date fix

--- tests/test_entry_dates.py
+++ tests/test_entry_dates.py
@@ -8,22 +8,28 @@ def test_entry_dates_wrapping_years():
         amount='123',
         status='D',
         year=2000,
-        month=12,
-        day=31,
     )
 
     # Regular statement
-    statement(transactions, dict(data.items()))
+    statement(transactions, dict(
+        data.items(),
+        month=1,
+        day=1,
+    ))
 
     # Statement which wraps to the future
     statement(transactions, dict(
         data.items(),
+        month=12,
+        day=31,
         entry_day=1,
         entry_month=1,
     ))
     # Statement which wraps the past year
     statement(transactions, dict(
         data.items(),
+        month=1,
+        day=1,
         entry_day=31,
         entry_month=12,
     ))
