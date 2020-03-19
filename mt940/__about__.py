972259f2f5383cc248925a6edf47ab8806f0a1ca
Rick van Hattem <Wolph@wol.ph>
2020-03-19 21:45:15 +0100

    Incrementing version to v4.21.0

--- mt940/__about__.py
+++ mt940/__about__.py
@@ -6,7 +6,7 @@ A library to parse MT940 files and returns smart Python collections for
 statistics and manipulation.
 '''.strip().split())
 __email__ = 'wolph@wol.ph'
-__version__ = '4.20.0'
+__version__ = '4.21.0'
 __license__ = 'BSD'
 __copyright__ = 'Copyright 2015 Rick van Hattem (wolph)'
 __url__ = 'https://github.com/WoLpH/mt940'
